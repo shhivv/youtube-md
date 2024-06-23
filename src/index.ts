f61a155c4a05ab52d1da6574d884a930162b9145
Shiv <shiv.7.shan@gmail.com>
2024-06-23 11:28:25 +0530

    write program

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,51 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { streamText } from "hono/streaming";
+import { YoutubeTranscript } from "youtube-transcript";
 
 type Bindings = {
-  [key in keyof CloudflareBindings]: CloudflareBindings[key]
-}
+  [key in keyof CloudflareBindings]: CloudflareBindings[key];
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  const url = c.req.query("url");
+  if (!url) {
+    return c.text('https://ytmd.shivs.me/url?="<url>"');
+  }
 
-export default app
+  try {
+    new URL(url);
+  } catch {
+    return c.text("invalid URL");
+  }
+  const transcript = (
+    await YoutubeTranscript.fetchTranscript(c.req.query("url") as string)
+  )
+    .map((i) => i.text)
+    .join(" ");
+  return streamText(c, async (stream) => {
+    const fetch = async (inp: string) =>
+      await c.env.AI.run("@cf/qwen/qwen1.5-14b-chat-awq", {
+        prompt: `You are responsible for summarizing transcript content to markdown. Given the transcript of a video, produce a readable, clean markdown with headings and subheadings based on the transcript content. 
+      Do not include any links.
+Input: ${inp}
+Output:\`\`\`markdown\n`,
+      });
+
+    const chunkSize = 5000;
+    const chunks = [];
+
+    for (let i = 0; i < transcript.length; i += chunkSize) {
+      chunks.push(transcript.slice(i, i + chunkSize));
+    }
+
+    for (const chunk of chunks) {
+      const response = await fetch(chunk);
+      // @ts-expect-error;
+      await stream.write(response.response);
+    }
+  });
+});
+
+export default app;
